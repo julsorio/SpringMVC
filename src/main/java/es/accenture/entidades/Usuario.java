b0b0f31a15d744081bd2a90c719453377a182820
julsorio <julian.osorio@gmail.com>
2024-10-03 07:13:02 +0200

    ajustes

---
+++ src/main/java/es/accenture/entidades/Usuario.java
@@ -0,0 +1,68 @@
+package es.accenture.entidades;
+
+public class Usuario {
+	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String password;
+	private String email;
+	private String direccion;
+	private String telefono;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
